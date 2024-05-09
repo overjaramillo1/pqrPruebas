1f615608a002243b06a8f1a2bb7cea8aa185311e
overjaramillo1 <jaramil161@gmail.com>
2024-05-09 13:09:08 -0500

    cambio codigo

--- src/app/components/shared/modal-alert/modal-alert.component.spec.ts
+++ src/app/components/shared/modal-alert/modal-alert.component.spec.ts
@@ -6,6 +6,16 @@ describe('ModalAlertComponent', () => {
   let component: ModalAlertComponent;
   let fixture: ComponentFixture<ModalAlertComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ModalAlertComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ModalAlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
